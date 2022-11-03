7d0d733408091d3fc48660017bacfff8ba48ba53
Arkazix <hugohamon27@gmail.com>
2022-11-03 20:11:22 +0100

    update game

--- project/src/game/game.py
+++ project/src/game/game.py
@@ -1,37 +1,40 @@
 from .direction import Direction
+# from maze.maze import Maze
 
 class Game:
 
     def __init__(self) -> None:
-        self.game_over = False
-        self.has_won = False
-        self.distance = 0
-        self.score = 0
+        # self.maze = Maze(filepath)
+        # self.pacman = Pacman(self.maze.get_start_pos())
+        pass
 
     # REQUESTS
     def is_game_over(self) -> bool:
         """Return True if the game is over"""
-        return self.game_over
+        # return self.maze.get_dots() == 0 or self.pacman.is_dead()
+        return False
 
     def is_game_won(self) -> bool:
         """Return True if the game is won"""
-        return self.has_won
+        # return self.maze.get_dots() == 0
+        return False
 
     def get_score(self) -> int:
         """Return the score"""
-        return self.score
+        # return self.pacman.get_score()
+        return 0
 
     def get_distance(self) -> int:
         """Return the distance"""
-        return self.distance
+        # return self.pacman.get_distance()
+        return 0
 
     # COMMANDS
     def reset(self) -> None:
         """Reset the game"""
-        self.game_over = False
-        self.has_won = False
-        self.distance = 0
-        self.score = 0
+        # self.maze.reset()
+        # self.pacman.reset()
+        pass
     
     def update(self) -> None:
         """Update the game"""
