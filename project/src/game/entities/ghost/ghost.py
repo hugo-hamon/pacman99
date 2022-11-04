459af52ddfb974f928fc39a779f5cbd8fbb37a8d
Pierrep02 <115887388+Pierrep02@users.noreply.github.com>
2022-11-04 22:17:32 +0100

    Update ghost.py

--- project/src/game/entities/ghost/ghost.py
+++ project/src/game/entities/ghost/ghost.py
@@ -1,12 +1,12 @@
 import abc
-import ghoststate as gs
+import .ghoststate as gs
 
 class generalGhost(abc.ABC) :
     
     def __init__(self, pacman_ref) :
         super().__init__()
-        pac_ref = pacman_ref
-        state = gs.CHASE
+        self.pac_ref = pacman_ref
+        self.state = gs.CHASE
         
     @abc.abstractmethod
     def _choose_direction(self, intersection) :
@@ -15,4 +15,10 @@ class generalGhost(abc.ABC) :
     
     def set_state(self, ghoststate : gs) -> None :
         """Set l'état du fantome"""
-        state = ghoststate
+        self.state = ghoststate
+        if ghoststate = gs.SCATTER :
+            __turnaround()
+            
+    def __turnaround() -> None :
+        #self.direction = self.direction.opposite TODO
+        pass
