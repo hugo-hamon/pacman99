53d4d08c3d5fa0ec94cb64be24b65875eb1fb6a4
Arkazix <hugohamon27@gmail.com>
2022-11-08 11:28:54 +0100

    direction vector

--- project/src/game/direction.py
+++ project/src/game/direction.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import Tuple
 from enum import Enum
 
 
@@ -11,3 +12,12 @@ class Direction(Enum):
     def opposite(self) -> Direction:
         """Return the opposite direction"""
         return Direction((self.value + 2) % 4)
+
+    def to_vector(self) -> Tuple[int, int]:
+        """Return the direction as a vector"""
+        return {
+            Direction.NORTH: (0, -1),
+            Direction.EAST: (1, 0),
+            Direction.SOUTH: (0, 1),
+            Direction.WEST: (-1, 0),
+        }[self]
