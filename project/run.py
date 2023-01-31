f8a6675fe9b56484d345efb02576d9fcec4b295e
pierrep02 <pierrep070225@gmail.com>
2023-01-31 17:31:17 +0100

    GraphicGame change

--- project/run.py
+++ project/run.py
@@ -1,4 +1,10 @@
 from src import main
+import sys
 
 if __name__ == '__main__':
-    main.run()
+    n = len(sys.argv)
+    if n == 2:
+        main.run(sys.argv[1])
+    else:
+        print("INFO : Using default config path")
+        main.run()
--- project/src/graphics/graphic.py
+++ project/src/graphics/graphic.py
@@ -10,19 +10,19 @@ from .menu import MainMenu
 from .sounds import Sounds
 import pygame as pg
 
-
 spritesheet_path = "assets/images/palettes.png"
 TILE_SIZE = 8
 
 
-class Graphic:
+class GraphicGame(Game):
 
-    def __init__(self, config: Config, game: Game, sounds: Sounds) -> None:
+    def __init__(self, config: Config, sounds: Sounds) -> None:
+        super().__init__(config)
         self.config = config
         factor = max(config.graphics.width, config.graphics.height) / \
-            (game.get_maze().get_width() * TILE_SIZE)
-        screen_width = game.get_maze().get_width() * TILE_SIZE * int(factor)
-        screen_height = game.get_maze().get_height() * TILE_SIZE * int(factor)
+            (self.get_maze().get_width() * TILE_SIZE)
+        screen_width = self.get_maze().get_width() * TILE_SIZE * int(factor)
+        screen_height = self.get_maze().get_height() * TILE_SIZE * int(factor)
         self.screen = pg.display.set_mode(
             (screen_width, screen_height))
         pg.display.set_caption(config.graphics.title)
@@ -34,7 +34,7 @@ class Graphic:
 
         self.spritesheet = SpriteSheet(spritesheet_path)
         self.fps = config.graphics.fps
-        self.game = game
+        self.game = self
         self.run = False
 
         # Create the sprites
@@ -70,7 +70,7 @@ class Graphic:
                     self.__process_key_event(event)
 
             # Update the game
-            self.game.update()
+            self.update()
 
             # Display the sprites
             self.display_sprites()
@@ -85,13 +85,13 @@ class Graphic:
     def __process_key_event(self, event: pg.event.Event) -> None:
         if not self.config.genetic.genetic_enable:
             if event.key == pg.K_q:
-                self.game.get_pacman().set_next_direction(Direction.WEST)
+                self.get_pacman().set_next_direction(Direction.WEST)
             if event.key == pg.K_d:
-                self.game.get_pacman().set_next_direction(Direction.EAST)
+                self.get_pacman().set_next_direction(Direction.EAST)
             if event.key == pg.K_z:
-                self.game.get_pacman().set_next_direction(Direction.NORTH)
+                self.get_pacman().set_next_direction(Direction.NORTH)
             if event.key == pg.K_s:
-                self.game.get_pacman().set_next_direction(Direction.SOUTH)
+                self.get_pacman().set_next_direction(Direction.SOUTH)
         if event.key == pg.K_ESCAPE and self.config.user.menu_enable:
             self.launch_main_menu(False)
             if self.config.user.sound_enable:
@@ -125,3 +125,43 @@ class Graphic:
         self.main_menu.run()
         if play_sound:
             self.sound.fadeout_sound(1000)
+"""
+    def eat_dot(self) -> None:
+        pac_pos = (round(self.pacman.get_position()[0]), round(
+            self.pacman.get_position()[1]))
+        if pac_pos[0] >= 0 and pac_pos[0] < self.maze.get_width() and \
+                pac_pos[1] >= 0 and pac_pos[1] < self.maze.get_height():
+            match self.maze.get_cell(pac_pos[0], pac_pos[1]):
+                case Components.DOT:
+                    if self.config.user.enable_graphics and self.config.user.sound_enable:
+                        self.sounds.play_sound_once("assets/music/munch_1.wav")
+                    self.score += DOT_SCORE
+                    self.maze.set_component(
+                        Components.EMPTY, pac_pos[1], pac_pos[0])
+                case Components.SUPERDOT:
+                    if self.config.user.enable_graphics and self.config.user.sound_enable:
+                        self.sounds.play_sound_once("assets/music/munch_2.wav")
+                        self.sounds.play_sound_once(
+                            "assets/music/power_pellet.wav")
+                    self.get_pacman().change_state()
+                    self.super_mode_timer = self.config.game.super_mode_duration * self.config.graphics.fps / self.config.game.game_speed
+                    for ghost in self.ghosts:
+                        if ghost.state != Ghoststate.EATEN:
+                            ghost.set_state(Ghoststate.FRIGHTENED)
+                    self.score += SUPER_DOT_SCORE
+                    self.maze.set_component(
+                        Components.EMPTY, pac_pos[1], pac_pos[0])
+
+    def __manage_collision(self, ghost: GeneralGhost) -> None:
+        if self.__is_pacman_ghost_colliding(ghost):
+            if ghost.state in [Ghoststate.FRIGHTENED, Ghoststate.EATEN]:
+                ghost.set_state(Ghoststate.EATEN)
+                ghost.set_speed(ghost.get_speed() * 4)
+                self.score += GHOST_SCORE
+            else:
+                if self.config.user.enable_graphics and self.config.user.sound_enable:
+                    self.sounds.play_sound_once("assets/music/death_1.wav")
+                self.pacman.lose_life()
+                self.respawn_pacman()
+                self.respawn_ghosts()
+"""
