6cfc344fafaad9108a362d077924a7dd39c80065
pierrep02 <pierrep070225@gmail.com>
2023-03-29 21:59:54 +0200

    t

--- project/src/game/geneticGame.py
+++ project/src/game/geneticGame.py
@@ -1,30 +1,30 @@
 from .maze.random_maze_factory import RandomMazeFactory
+from ..utils.eventBroadcast import EventBroadcast
 from typing import List, Tuple, Union
 from .maze.maze import Maze
 from ..config import Config
 import math
 from .game import Game
+from ..graphics.sounds import Sounds
 
-class GeneticManager():
-    """Class allowing multiple games to be played at the same time with genetic algorithms"""
-    def __init__(self, config: Config, maze_Nb):
+class GeneticGame():
+    """Class allowing a game controlled by genetic algorithm"""
+    def __init__(self, config: Config, sounds: Sounds=None) -> None:
+        """If sounds is set creates a graphic game otherwise creates a normal game"""
         path = self.config.graphics.maze_path
         if self.config.user.enable_random_maze:
             RandomMazeFactory(self.config).create()
             path = self.config.maze.random_maze_path
         self.maze = Maze(path)
-        self.games = []
+        self.games = Game(config, self.maze, getNextMove) if 
         self.movesList = [[]] * maze_Nb
         #Initialiser Buffer?
     
     def setMovements(self, movesList: List):
         assert(len(movesList) == len(self.movesList))
-        self.movesList = movesList
+        self.moveList = movesList
 
-    def setStartingMoves(self, moveList: List):
-        self.movesList = [[moveList]] * len(self.movesList)
-
-    def runGames(self):
+    def runGame(self):
         #TODO Do buffer shenanigans
         """Résumé : Buffer 
                     Début parallélisation
@@ -34,3 +34,6 @@ class GeneticManager():
                     On met toute les games dans le buffer
                     """
         return self.games
+
+    def getNextMove(self, _):
+
